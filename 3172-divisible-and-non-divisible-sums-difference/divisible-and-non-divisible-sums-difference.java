1ec9761023e111904a1626213c9a32520b8c30d8
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2025-05-27 23:34:34 +0530

    Time: 1 ms (74.69%) | Memory: 41.2 MB (11.01%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,28 +1,39 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-    //     int sum1=0;
-    //     int sum2=0;
-    //     int []arr=new int[n];
-    //     for(int i=0;i<n;i++){
-    //         if( n%m!=0 ){
-    //             sum1+=arr[i];
-    //         }
-    //         else{
-    //             sum2+=arr[i];
+        int count=0;
+        int x=0;
+        
+        for(int i=1;i<=n;i++){
+            if( i%m!=0 ){
+                count+=i;
+            }
+            else{
+                x+=i;
 
-    //         }
-    //     }
+            }}
+    return count-x;
+        }}
     
     //    return sum1-sum2;
+    //  int count=0;
+    //     int c1=0;
+    //     for(int i=1;i<=n;i++){
+    //         if(i%m==0){
+    //             count+=i;
+    //         }
+    //         else{
+    //             c1+=i;
+    //         }
+    //     }
+    //     return c1-count;
 
-    int totalSum = n * (n + 1) / 2;
-        int count = n / m;
-        int divisibleSum = m * count * (count + 1) / 2;
+    // int totalSum = n * (n + 1) / 2;
+    //     int count = n / m;
+    //     int divisibleSum = m * count * (count + 1) / 2;
 
-        int num1 = totalSum - divisibleSum;
-        int num2 = divisibleSum;
+    //     int num1 = totalSum - divisibleSum;
+    //     int num2 = divisibleSum;
 
-        return num1 - num2; 
+    //     return num1 - num2; 
 
-    }
-}
+    
