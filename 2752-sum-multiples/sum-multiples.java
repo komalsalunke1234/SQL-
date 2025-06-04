576eaaeac9eb2a72fcd34f3b77c7e082a3cfd72a
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2025-06-04 23:32:39 +0530

    Time: 3 ms (60.46%) | Memory: 40.8 MB (66.99%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -1,11 +1,11 @@
 class Solution {
     public int sumOfMultiples(int n) {
         int sum=0;
-        int count=0;
+        
         for(int i=1;i<=n;i++){
             if(i%3==0 || i%5==0 || i%7==0){
                 
-                count++;
+                
                 sum=sum+i;
 
             }
