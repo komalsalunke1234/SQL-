6d690de8f1547f3028de46f4b06f4bcad73b065a
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2026-01-27 23:08:01 +0530

    Time: 89 ms (11.11%) | Memory: 47.5 MB (91.75%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int []nge=new int[n];
+        Arrays.fill(nge,-1);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<i+n;j++){
+                int index=j%n;
+                if(nums[index]>nums[i]){
+                    nge[i]=nums[index];
+                    break;
+                }
+            }
+        }
+        return nge;
+    }
+}
